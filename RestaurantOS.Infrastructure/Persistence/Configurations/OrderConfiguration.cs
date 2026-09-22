b061f4d84c4982bb4f784306fd9ab58db0845d9e
Davit <davitshengelia21@gmail.com>
2026-09-22 13:42:36 +0200

    Add RestaurantDbContext and OrderConfiguration classes
    Introduced RestaurantDbContext with DbSets for core entities and model configuration. Added an empty OrderConfiguration class as a placeholder for future settings.

---
+++ RestaurantOS.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantOS.Infrastructure.Persistence.Configurations
+{
+    internal class OrderConfiguration
+    {
+    }
+}
---
+++ RestaurantOS.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantOS.Domain.Entities;
+
+namespace RestaurantOS.Infrastructure.Persistence;
+
+public class RestaurantDbContext : DbContext
+{
+    public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options)
+    : base(options)
+    {
+    }
+     public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<Table> Tables => Set<Table>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+    public DbSet<Reservation> Reservations => Set<Reservation>();
+    public DbSet<Payment> Payments => Set<Payment>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<MenuCategory> MenuCategories => Set<MenuCategory>();
+    public DbSet<InventoryItem> InventoryItems => Set<InventoryItem>();
+    public DbSet<Shift> Shifts => Set<Shift>();
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(RestaurantDbContext).Assembly);
+    }
+}
