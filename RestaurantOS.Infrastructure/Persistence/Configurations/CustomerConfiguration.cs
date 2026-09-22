3a5024c7aaecedaa489a3fa39e378c2878629640
Davit <davitshengelia21@gmail.com>
2026-09-23 01:46:16 +0200

    Add EF Core configs for Customer and User entities
    Added CustomerConfiguration and UserConfiguration classes to define table mapping, primary keys, property constraints, unique email indexes (excluding soft-deleted records), ignored FullName property, and global query filters for IsDeleted.

---
+++ RestaurantOS.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestaurantOS.Domain.Entities;
+
+namespace RestaurantOS.Infrastructure.Persistence.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.ToTable("Customers");
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(c => c.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Ignore(c => c.FullName);
+
+        builder.Property(c => c.Email)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.HasIndex(c => c.Email)
+            .IsUnique()
+            .HasFilter("\"IsDeleted\" = false");
+
+        builder.Property(c => c.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(c => c.PhoneNumber)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.HasQueryFilter(c => !c.IsDeleted);
+    }
+}
---
+++ RestaurantOS.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantOS.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RestaurantOS.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Ignore(u => u.FullName);
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.HasIndex(u => u.Email)
+            .IsUnique()
+            .HasFilter("\"IsDeleted\" = false");
+
+        builder.Property(u => u.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(u => u.Role)
+            .IsRequired();
+        builder.HasQueryFilter(u => !u.IsDeleted);
+    }
+}
